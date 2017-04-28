98c0fa06811bc6f18547bff748251d636d46177a
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-04-28 15:46:49 +0100

    Environment update tasks have been separated New published task to add the VSTS variables to the Chef environment
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- tasks/env_version_constraint/env_version_constraint.ts
+++ tasks/env_version_constraint/env_version_constraint.ts
@@ -11,7 +11,7 @@ import * as chefapi from "./common/chefapi";
 
 import {sprintf} from "sprintf-js";
 
-function update_environment(params, environment) {
+function set_cookbook_version_constraint(params, environment) {
 
   // ensure that the cookbook versions is set on the object
   if (!("cookbook_versions" in environment)) {
@@ -19,49 +19,6 @@ function update_environment(params, environment) {
   }
 
   environment["cookbook_versions"][params["chefCookbookName"]] = params["chefCookbookVersion"];
-
-  // determine if adding environment attributes
-  if (params["addEnvironmentAttributes"]) {
-
-    // create an array of variable names that should be not included
-    let not_include = ["agent", "release", "system", "build", "task", "MSDEPLOY_HTTP_USER_AGENT", "AZURE_HTTP_USER_AGENT", "requestedForId"];
-
-    // iterate around the variables for the release environment
-    let include = true;
-    let release_env_vars = tl.getVariables();
-    release_env_vars.forEach(function (release_env_var) {
-
-      // reset the include var
-      include = true;
-
-      // determine if the varible name begins with one of the not_include vars
-      not_include.forEach(function (start_string) {
-        if (release_env_var.name.startsWith(start_string)) {
-          include = false;
-        };
-      });
-
-      // only add to the environment of the include is true
-      if (include && !release_env_var.secret) {
-
-        // ensure that the environment has override attributes
-        if (!("override_attributes" in environment)) {
-          environment["override_attributes"] = {};
-        }
-
-        // ensure that the namespace is in the override_attributes
-        if (!(params["chefEnvironmentNamespace"] in environment["override_attributes"])) {
-          environment["override_attributes"][params["chefEnvironmentNamespace"]] = {};
-        }
-
-        // Now add the variable and its value to the environment
-        environment["override_attributes"][params["chefEnvironmentNamespace"]][release_env_var.name] = release_env_var.value;
-
-        console.log("Added variable to Chef environment: %s", release_env_var.name);
-      };
-
-    });
-  }
 
   // return the envrionment so that it can be sent
   return environment;
@@ -78,7 +35,7 @@ async function run() {
   let path = sprintf("environments/%s", params["chefEnvName"]);
 
   chefapi.call(tl, params, path, "get", "")
-    .then(update_environment.bind(null, params))
+    .then(set_cookbook_version_constraint.bind(null, params))
     .then(chefapi.call.bind(null, tl, params, path, "put"))
     .then(function (response) {
       console.log("Environment Constraints Updated");
---
+++ tasks/env_vsts_variables/env_vsts_variables.ts
@@ -0,0 +1,76 @@
+
+// Import library to support promises
+import * as Q from "q";
+
+// Import tasks from vsts
+import * as tl from "vsts-task-lib/task";
+
+// Import common tasks
+import * as inputs from "./common/inputs";
+import * as chefapi from "./common/chefapi";
+
+import {sprintf} from "sprintf-js";
+
+function add_vsts_variables_to_env(params, environment) {
+
+    // create an array of variable names that should be not included
+    let not_include = ["agent", "release", "system", "build", "task", "MSDEPLOY_HTTP_USER_AGENT", "AZURE_HTTP_USER_AGENT", "requestedForId"];
+
+    // iterate around the variables for the release environment
+    let include = true;
+    let release_env_vars = tl.getVariables();
+    release_env_vars.forEach(function (release_env_var) {
+
+      // reset the include var
+      include = true;
+
+      // determine if the varible name begins with one of the not_include vars
+      not_include.forEach(function (start_string) {
+        if (release_env_var.name.startsWith(start_string)) {
+          include = false;
+        };
+      });
+
+      // only add to the environment of the include is true
+      if (include && !release_env_var.secret) {
+
+        // ensure that the environment has override attributes
+        if (!("override_attributes" in environment)) {
+          environment["override_attributes"] = {};
+        }
+
+        // ensure that the namespace is in the override_attributes
+        if (!(params["chefEnvironmentNamespace"] in environment["override_attributes"])) {
+          environment["override_attributes"][params["chefEnvironmentNamespace"]] = {};
+        }
+
+        // Now add the variable and its value to the environment
+        environment["override_attributes"][params["chefEnvironmentNamespace"]][release_env_var.name] = release_env_var.value;
+
+        console.log("Added variable to Chef environment: %s", release_env_var.name);
+      };
+
+    });
+  }
+
+/** Asynchronous function to update environment */
+async function run() {
+
+  // Get the parameters that have been set on the task
+  let params = inputs.parse(process, tl);
+
+  // set the path that is to be called
+  let path = sprintf("environments/%s", params["chefEnvName"]);
+
+  chefapi.call(tl, params, path, "get", "")
+    .then(add_vsts_variables_to_env.bind(null, params))
+    .then(chefapi.call.bind(null, tl, params, path, "put"))
+    .then(function (response) {
+      console.log("Environment variables added");
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+run();
