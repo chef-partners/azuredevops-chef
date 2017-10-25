4ac3a609682d1c1977dfbbf5c8c215a6cecac6b4
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-10-25 12:17:07 +0100

    Updated to handle supermarket with self signed certs
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- tasks/publish_supermarket/publish_supermarket.ts
+++ tasks/publish_supermarket/publish_supermarket.ts
@@ -13,15 +13,40 @@ import * as utils from "./common/utils";
 
 async function run() {
 
+    // set the knife cmd to run
+    let cmd = "/opt/chefdk/bin/knife";
+
     // ensure that chefdk is installed
     utils.installChefDK(tl, fs);
 
     // Get the parameters that have been set on the task
     let params = inputs.parse("chefSupermarketEndpoint", process, tl);
 
+    // the private key needs to be written out to a file
+    let key_filename: string = sprintf("/tmp/supermarket_%s.pem", params['chefUsername']);
+    try {
+        fs.writeFileSync(key_filename, params['chefKeyFile']);
+    } catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+
+    // if the task is not to perform an SSL checks then the certs need to be 
+    // downloaded from the chef server
+    if (!params['chefSSLVerify']) {
+        console.log("Fetching SSL certificates: %s", params['chefServiceUrl']);
+
+        // set the agruments to download the SSL certificates
+        let args = sprintf("ssl fetch %s -u %s -k %s", params["chefServiceUrl"], params["chefUsername"], key_filename)
+
+        try {
+            let exit_code: number = await tl.tool(cmd).line(args).exec();
+        } catch (err) {
+            tl.setResult(tl.TaskResult.Failed, err.message);
+        }
+    }
+
     // set the command and the arguments that are to be run
-    let cmd = "/opt/chefdk/bin/knife";
-    let args = sprintf("cookbook site share %s -o %s -m %s -u %s -k %s", params["chefCookbookName"], params["chefCookbookPath"], params["chefUsername"], params["chefUserKey"]);
+    let args = sprintf("cookbook site share %s -o %s -m %s -u %s -k %s", params["chefCookbookName"], params["chefCookbookPath"], params["chefServiceUrl"], params["chefUsername"], key_filename);
 
     // publish the named cookbook to the supermaket
     try {
