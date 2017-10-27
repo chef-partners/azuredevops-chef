f9f85e510e2e620d3ba2e9ce1a0dc4de86446ba6
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-10-27 11:56:27 +0100

    Setting boolean value for the SSL verify
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- tasks/common/inputs.ts
+++ tasks/common/inputs.ts
@@ -49,7 +49,7 @@ export function parse(serviceEndpointName, process, tl) {
           if (serviceEndpointName == "chefServerEndpoint") {
             inputs["chefSSLVerify"] = !!+auth.parameters.sslVerify;
           } else {
-            inputs["chefSSLVerify"] = tl.getEndpointDataParameter(serviceEndpointName, "sslVerify", true);
+            inputs["chefSSLVerify"] = !!+tl.getEndpointDataParameter(serviceEndpointName, "sslVerify", true);
           }
 
           tl.debug(sprintf("SSL Verify: %s", inputs["chefSSLVerify"]));
