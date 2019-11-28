9f392a7fb8dcb12fc4ba52b8580b54b3205dfcee
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2019-11-28 17:09:42 +0000

    Corrected taskDir path
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- src/install/install.ts
+++ src/install/install.ts
@@ -1,12 +1,15 @@
 // Import the necessary tasks
 import * as task from "./common/taskConfiguration";
 import * as ic from "./common/installComponents";
+import { basename } from "path";
 
 // Perform the install operation
 async function run() {
 
   // initialise required classes
-  let taskConfiguration = new task.TaskConfiguration(__dirname);
+  // the basename of __filename is used rather than __dirname because when it 
+  // is run under the Azure DevOps agent __dirname resolves to /
+  let taskConfiguration = new task.TaskConfiguration(basename(__filename));
 
   await taskConfiguration.getTaskParameters();
 
